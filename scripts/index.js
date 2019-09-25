39e8438b7d75ce5e21169ad7da4cbd7b1a4c57a4
Bridger <bridgerrhammond@gmail.com>
2019-09-25 09:59:35 -0500

    #3

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,19 @@
 import item from './item.js';
 import shoppingList from './shopping-list.js';
+import store from './store.js';
 
 const main = function () {
+  // const itemNames = ['', 'apples', 'pears'];
+  // itemNames.forEach(name => {
+  //   try {
+  //     item.validateName(name);
+  //     store.items.push(item.create(name));
+  //   } catch (error) {
+  //     console.log(`Cannot add item: ${error.message}`);
+  //   }
+  // })
+  //  Testing valiedateName() and create()
+
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
